ae202e6acc0f243515b4878f8be64ec83208539f
evanth-h <61976772+evanth-h@users.noreply.github.com>
2020-05-13 14:53:57 +0530

    Add project files.

---
+++ WebAPITest/Controllers/ValuesController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WebAPITest.Models;
+using static WebAPITest.Models.PersonParameter;
+
+namespace WebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value - " + id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpPost("covidTest")]
+        public IActionResult CheckPersonSymptoms([FromBody] PersonParameter personParameter)
+        {
+
+            string result = string.Empty;
+
+            if (personParameter != null)
+            {
+                if (personParameter.IsFever && personParameter.IsHavingCough && personParameter.IsVistedOutsideIndia)
+                {
+                    result = Colors.Red.ToString();
+                }
+                else if (!personParameter.IsFever && !personParameter.IsHavingCough && !personParameter.IsVistedOutsideIndia)
+                {
+                    result = Colors.Green.ToString();
+                }
+                else if ((personParameter.IsFever && personParameter.IsHavingCough) || (personParameter.IsVistedOutsideIndia && personParameter.IsFever) || (personParameter.IsHavingCough && personParameter.IsVistedOutsideIndia))
+                {
+                    result = Colors.Orange.ToString();
+                }
+            }
+
+            //Soltion 2
+            int sum = 0;
+            if (personParameter != null)
+            {
+
+                sum = Convert.ToInt32(personParameter.IsFever) + Convert.ToInt32(personParameter.IsHavingCough) + Convert.ToInt32(personParameter.IsVistedOutsideIndia);
+            }
+            if (sum > 2)
+            {
+                result = Colors.Red.ToString();
+            }
+            else if (sum <= 2 && sum > 0)
+            {
+                result = Colors.Orange.ToString();
+            }
+            else if (sum == 0)
+            {
+                result = Colors.Green.ToString();
+            }
+
+            return Ok(result);
+        }
+
+
+        [HttpGet("testException")]
+        public IActionResult GlobalException()
+        {
+            int[] ar = null;
+
+            if (ar.Length > 0)
+            {
+
+            }
+            return Ok();
+            //string filePath = @"C:\Users\EJain\Desktop\test.txt";
+            //System.IO.File.AppendAllText(filePath, " " + result);
+            //var read = System.IO.File.ReadAllText(filePath);
+        }
+    }
+}
---
+++ WebAPITest/Models/PersonParameter.cs
@@ -0,0 +1,21 @@
+﻿namespace WebAPITest.Models
+{
+    public class PersonParameter
+    {
+        public bool IsVistedOutsideIndia { get; set; }
+        public bool IsHavingCough { get; set; }
+        public bool IsFever { get; set; }
+
+        public bool MyProperty { get; set; }
+        public bool MyProperty1 { get; set; }
+
+
+        public enum Colors
+        {
+            Red,
+            Orange,
+            Green
+
+        }
+    }
+}
